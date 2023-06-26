1de3e5655e5d7ef185f6bc74ec884e26dcd41032
Sean <ohsean812@gmail.com>
2023-06-26 00:08:37 -0400

    deliverable 1 done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    const form = document.querySelector("form")
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const taskText = event.target["new-task-description"].value + " "
+        
+        const li = document.createElement("li")
+        li.innerText = taskText
+
+        const button = document.createElement("button")
+        button.innerText = "X"
+        li.appendChild(button)
+
+        button.addEventListener("click", function(event) {
+            event.target.parentNode.remove()
+        })
+
+        const taskList = document.getElementById("tasks")
+        taskList.appendChild(li)
+        
+    })
+
 });
